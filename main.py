cfdfbfd3a9d4786a9dd0c0d30f80da82bc9d0a97
Ivan Ovchinikov <ivan13air@mail.ru>
2024-09-29 13:30:52 +0300

    Это наш первый коммит в проекте

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+import pprint
+from typing import Tuple
+
+print("Hello world!")
+
+
+def foo(x: int) -> Tuple[int, float]:
+    return x, math.pi
+
+
+pprint.pprint('Hello')
+
+
+print(foo(4))
